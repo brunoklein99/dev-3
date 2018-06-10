86cc19f1203b4f6bf21f5b45c9ba87cbe5c58f97
Rafael Eyng <rafaeleyng@gmail.com>
2018-06-10 13:20:15 -0300

    fix script

--- academia-db/client-web.sh
+++ academia-db/client-web.sh
@@ -3,4 +3,4 @@
 # tenta apagar um container pré-existente. Se não existir, esse comando irá falhar, mas não tem problema
 docker rm -f academia-mysql-client-web
 
-docker run --network=academia --link academia-mysql:db -p 9000:8080 adminer
+docker run -d --network=academia --link academia-mysql:db --name academia-mysql-client-web -p 9000:8080 adminer
